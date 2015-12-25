4032ce38f8382fd377ce8db54e33a83db21405b8
Iurii Kobernyk <koiu@ciklum.com>
2015-12-25 19:11:18 +0200

    add branch chat

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,25 +4,27 @@
 export const REQUEST_DATA = "REQUEST_DATA";
 export const RECEIVE_DATA = "RECEIVE_DATA";
 
-export const requestData = (): Object => {
-  return {
-    type: REQUEST_DATA
-  };
+export const requestData = ():Object => {
+	return {
+		type: REQUEST_DATA
+	};
 };
 
-export const receiveData = (data: Object): Object => {
-  return {
-    type: RECEIVE_DATA,
-    data
-  };
+export const receiveData = (data:Object):Object => {
+	return {
+		type: RECEIVE_DATA,
+		data
+	};
 };
 
-export const fetchData = (): Function => {
-  return (dispatch) => {
-    dispatch(requestData());
-    return setTimeout(() => {
-      const data = {message: "Hello world!"};
-      dispatch(receiveData(data));
-    }, 300);
-  };
+export const fetchData = ():Function => {
+	return (dispatch) => {
+
+		dispatch(requestData());
+
+		return setTimeout(() => {
+			const data = { message: "Hello world!!" };
+			dispatch(receiveData(data));
+		}, 2300);
+	};
 };
--- src/containers/app.js
+++ src/containers/app.js
@@ -6,42 +6,43 @@ import { connect } from "react-redux/native";
 import { fetchData } from "../actions";
 
 let {
-  Text,
-  ScrollView
-} = React;
+	Text,
+	ScrollView
+	} = React;
 
 class App extends React.Component {
-  componentDidMount() {
-    this.props.dispatch(fetchData());
-  }
-  render() {
-    return (
-      <ScrollView
-        style={{flex: 1}}
-        contentContainerStyle={{
-          justifyContent: "center",
-          alignItems: "center"
-        }}
-      >
-        <Text>{this.props.isFetching ? "Loading" : this.props.message}</Text>
-      </ScrollView>
-    );
-  }
+	componentDidMount() {
+		this.props.dispatch(fetchData());
+	}
+
+	render() {
+		return (
+			<ScrollView
+				style={{flex: 1}}
+				contentContainerStyle={{
+					justifyContent: "center",
+                    alignItems: "center"
+                }}>
+				<Text>{this.props.isFetching ? "Loading" : this.props.message}</Text>
+			</ScrollView>
+		);
+	}
 }
 
 App.propTypes = {
-  dispatch: React.PropTypes.func,
-  message: React.PropTypes.string,
-  isFetching: React.PropTypes.bool
+	dispatch: React.PropTypes.func,
+	message: React.PropTypes.string,
+	isFetching: React.PropTypes.bool
 };
 
 App.defaultProps = {
-  dispatch: () => {},
-  isFetching: false,
-  message: ""
+	dispatch: () => {
+	},
+	isFetching: false,
+	message: ""
 };
 
 export default connect((state) => ({
-  isFetching: state.data.isFetching,
-  message: state.data.message
+	isFetching: state.data.isFetching,
+	message: state.data.message
 }))(App);
